7a054e8e6524c40d8fcebd05f2c1400afe1a9407
jawn1890 <59125607+jawn1890@users.noreply.github.com>
2024-10-31 20:27:10 -0400

    made the platform rotate!
    still tweaking it though.

--- 3DGameProject001/Assets/Scripts/RotatePlatform.cs
+++ 3DGameProject001/Assets/Scripts/RotatePlatform.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RotatePlatform : MonoBehaviour
 {
+    public Vector2 spin;
+    //public float spinSpeed = 2f;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,20 @@ public class RotatePlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      
+            if (Input.GetAxis("Mouse X") > 0 || Input.GetAxis("Mouse X") < 0)
+            {
+                
+                spin.x += Input.GetAxis("Mouse X");
+                transform.localRotation = Quaternion.Euler(0f, -spin.x, 0f);
+                Debug.Log("column spinning!");
+
+            }
+            else
+            {
+                Debug.Log("column stopped!!");
+            }
+
     }
+
 }
