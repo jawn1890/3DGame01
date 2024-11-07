09cf9d6e75ce46adeb3b4e9af70c20e56b21f462
jawn1890 <59125607+jawn1890@users.noreply.github.com>
2024-11-07 18:25:12 -0500

    added a dollar bill texture and made some collision stuff happen

--- 3DGameProject001/Assets/Scripts/SpawnNewPlatform.cs
+++ 3DGameProject001/Assets/Scripts/SpawnNewPlatform.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class SpawnNewPlatform : MonoBehaviour
 {
+    
+    public GameObject newPlatform;
+
+    public Vector3 startPos;
+    public Vector3 newPos;
+    public float xPos;
+    public float yPos;
+    public float zPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        yPos = transform.position.y;
+        startPos = new Vector3(transform.position.x, yPos, transform.position.z);
     }
 
     // Update is called once per frame
@@ -15,4 +24,14 @@ public class SpawnNewPlatform : MonoBehaviour
     {
         
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        yPos++;
+        newPos = new Vector3(transform.position.x, yPos, transform.position.z);
+        Debug.Log(newPos);
+        Instantiate(newPlatform, newPos, Quaternion.identity);
+
+    }
+    
 }
